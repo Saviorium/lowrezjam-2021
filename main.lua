a4c5222a79d6ecdbd4efd3ce0948b9b5e7357a5d
savior <saviorium@gmail.com>
2021-08-01 00:10:08 +0300

    Prepare lowrez pixel canvas

--- main.lua
+++ main.lua
@@ -18,6 +18,7 @@ states = {
 
 fonts = {}
 
+local pixelCanvas = require "engine.render.global_pixel_canvas" (Vector(64, 64), 4)
 
 function love.load()
     AssetManager:load("data")
@@ -25,14 +26,18 @@ function love.load()
 end
 
 function love.draw()
-    StateManager.draw()
-    if Debug and Debug.showFps == 1 then
-        love.graphics.print(""..tostring(love.timer.getFPS( )), 2, 2)
-    end
-    if Debug and Debug.mousePos == 1 then
-        local x, y = love.mouse.getPosition()
-        love.graphics.print(""..tostring(x)..","..tostring(y), 2, 32)
-    end
+    pixelCanvas:renderTo( function()
+        love.graphics.clear({0,0,0,1})
+        StateManager.draw()
+        if Debug and Debug.showFps == 1 then
+            love.graphics.print(""..tostring(love.timer.getFPS( )), 2, 2)
+        end
+        if Debug and Debug.mousePos == 1 then
+            local x, y = love.mouse.getPosition()
+            love.graphics.print(""..tostring(x)..","..tostring(y), 2, 32)
+        end
+    end )
+    pixelCanvas:draw()
 end
 
 function love.update(dt)
@@ -53,6 +58,10 @@ function love.mousereleased(x, y)
 end
 
 function love.keypressed(key)
+    if key == "1" or key == "2" or key == "3" or key == "4" or key == "5" then
+        local scale = tonumber(key)
+        pixelCanvas:setScale(scale)
+    end
     if StateManager.current().keypressed then
         StateManager.current():keypressed(key)
     end
